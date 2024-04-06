f90715978bd7e7c35279694e63253334162b0f81
farkmu45 <farukmicros@outlook.com>
2024-04-06 15:00:01 +0800

    Add achievements page

---
+++ app/Filament/Mentor/Pages/Achievements.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Filament\Mentor\Pages;
+
+use App\Models\Achievement;
+use Filament\Pages\Page;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Concerns\InteractsWithTable;
+use Filament\Tables\Contracts\HasTable;
+use Filament\Tables\Table;
+
+class Achievements extends Page implements HasTable
+{
+    use InteractsWithTable;
+
+    protected static string $view = 'filament.mentor.pages.achievements';
+
+    protected static ?string $navigationIcon = 'heroicon-o-users';
+
+    protected static ?string $title = 'Student Achievement';
+
+
+    public function table(Table $table): Table
+    {
+        return $table
+            ->query(Achievement::query())
+            ->columns([
+                TextColumn::make('no')
+                    ->label('No.')
+                    ->rowIndex(),
+                TextColumn::make('created_at')
+                    ->hidden(),
+                TextColumn::make('student.name')
+                    ->label('Name')
+                    ->searchable()
+                    ->sortable(),
+                TextColumn::make('student.matrix')
+                    ->label('Matrix')
+                    ->searchable(),
+                TextColumn::make('details')
+                    ->label('Achievement')
+                    ->wrap()
+            ])
+            ->defaultSort('created_at', 'desc')
+            ->filters([
+                // ...
+            ])
+            ->actions([
+                // ...
+            ])
+            ->bulkActions([
+                // ...
+            ]);
+    }
+}
