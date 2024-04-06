cc78777ed8ead747a154356ed1066b68cb227c4f
farkmu45 <farukmicros@outlook.com>
2024-04-06 14:48:39 +0800

    Add student data

---
+++ app/Filament/Mentor/Pages/Students.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Filament\Mentor\Pages;
+
+use App\Models\Student as ModelsStudent;
+use Filament\Pages\Page;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Concerns\InteractsWithTable;
+use Filament\Tables\Contracts\HasTable;
+use Filament\Tables\Table;
+
+class Students extends Page implements HasTable
+{
+    use InteractsWithTable;
+
+    protected static string $view = 'filament.mentor.pages.student';
+
+    protected static ?string $title = 'Student Data';
+
+    protected static ?string $navigationIcon = 'heroicon-o-users';
+
+    public function table(Table $table): Table
+    {
+        return $table
+            ->query(ModelsStudent::query())
+            ->columns([
+                TextColumn::make('no')
+                    ->label('No.')
+                    ->rowIndex(),
+                TextColumn::make('id')
+                    ->hidden(),
+                TextColumn::make('name')
+                    ->searchable()
+                    ->sortable(),
+                TextColumn::make('matrix')
+                    ->searchable(),
+                TextColumn::make('semester')
+                    ->sortable(),
+                TextColumn::make('country.name')
+                    ->sortable()
+                    ->searchable(),
+            ])
+            ->defaultSort('id', 'desc')
+            ->filters([
+                // ...
+            ])
+            ->actions([
+                // ...
+            ])
+            ->bulkActions([
+                // ...
+            ]);
+    }
+}
