45dc68cfc701167a3fbe3e6a0af749bf42b072ab
farkmu45 <farukmicros@outlook.com>
2024-04-22 17:56:20 +0800

    Make achievements multiple

--- app/Filament/Mentor/Resources/AchievementResource.php
+++ app/Filament/Mentor/Resources/AchievementResource.php
@@ -2,13 +2,11 @@
 
 namespace App\Filament\Mentor\Resources;
 
-use App\Filament\Mentor\Resources\CriminalHistoryResource\Pages;
-use App\Filament\Mentor\Resources\CriminalHistoryResource\RelationManagers;
-use App\Models\CriminalHistory;
+use App\Filament\Mentor\Resources\AchievementResource\Pages;
+use App\Filament\Mentor\Resources\AchievementResource\RelationManagers;
+use App\Models\Achievement;
 use Filament\Forms;
 use Filament\Forms\Form;
-use Filament\Infolists\Components\TextEntry;
-use Filament\Infolists\Infolist;
 use Filament\Resources\Resource;
 use Filament\Tables;
 use Filament\Tables\Columns\TextColumn;
@@ -16,17 +14,13 @@ use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
 
-class CriminalHistoryResource extends Resource
+class AchievementResource extends Resource
 {
-    protected static ?string $model = CriminalHistory::class;
+    protected static ?string $model = Achievement::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-scale';
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
 
-    protected static ?string $slug = 'criminal-history';
-
-    protected static ?string $pluralLabel = 'criminal history';
-
-    protected static ?string $label = 'criminal history';
+    protected static ?string $title = 'Student Achievement';
 
     public static function canCreate(): bool
     {
@@ -38,20 +32,6 @@ class CriminalHistoryResource extends Resource
         return parent::getEloquentQuery()->whereHas('student', function($query) {
             $query->where('mentor_id', auth()->user()->id);
          });
-    }
-
-
-
-    public static function infolist(Infolist $infolist): Infolist
-    {
-        return $infolist
-            ->schema([
-                TextEntry::make('student.name'),
-                TextEntry::make('student.matrix')
-                    ->label('Matrix'),
-                TextEntry::make('details')
-                    ->columnSpanFull(),
-            ]);
     }
 
     public static function table(Table $table): Table
@@ -69,7 +49,7 @@ class CriminalHistoryResource extends Resource
                     ->label('Matrix')
                     ->searchable(),
                 TextColumn::make('details')
-                    ->label('Criminal History')
+                    ->label('Achievement')
                     ->limit(20),
             ])
             ->filters([
@@ -77,13 +57,20 @@ class CriminalHistoryResource extends Resource
             ])
             ->actions([
                 Tables\Actions\ViewAction::make(),
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
             ]);
     }
 
     public static function getPages(): array
     {
         return [
-            'index' => Pages\ManageCriminalHistories::route('/'),
+            'index' => Pages\ManageAchievements::route('/'),
         ];
     }
 }
