19ce162390e78f1524d9aac86176443a6f2a95ca
farkmu45 <farukmicros@outlook.com>
2024-04-24 17:12:11 +0800

    Change achievement icon

--- app/Filament/Mentor/Resources/AchievementResource.php
+++ app/Filament/Mentor/Resources/AchievementResource.php
@@ -16,7 +16,7 @@ class AchievementResource extends Resource
 {
     protected static ?string $model = Achievement::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?string $navigationIcon = 'heroicon-o-trophy';
 
     protected static ?string $title = 'Student Achievement';
 
